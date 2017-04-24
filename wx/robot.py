1eac5e9d0637bbc5f3488c3c2d31fc8cf25846f1
willtunner <hellohanke@outlook.com>
2017-04-24 13:34:25 +0800

    bug fixed

--- wx/robot.py
+++ wx/robot.py
@@ -1,13 +1,21 @@
+from django.shortcuts import Http404
 from  werobot import WeRoBot
-from wx.wx_config import *
+from wx.wx_config import ENCODING_AES_KEY, TOKEN, APP_ID, APP_SECRET, SESSION_STORAGE
 
-robot = WeRoBot(APP_SERCRET=APP_SECRET,
-                token = TOKEN,
-                APP_ID=APP_ID,
-                ENCODING_AES_KEY=ENCODING_AES_KEY,
-                )
+robot = WeRoBot(
+    token=TOKEN,
+    app_id=APP_ID,
+    app_secret=APP_SECRET,
+    encoding_aes_key=ENCODING_AES_KEY,
+    enable_session=SESSION_STORAGE,
+)
 
 
 @robot.handler
 def hello(message):
     return 'Hello World!'
+
+
+@robot.error_page
+def make_error_page(url):
+    raise Http404
