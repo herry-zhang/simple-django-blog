f749dead537535aa28bd91abe536b5d07e4c900f
willtunner <hellohanke@outlook.com>
2017-04-30 00:18:46 +0800

    bug fixed

--- article/admin.py
+++ article/admin.py
@@ -10,11 +10,6 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'views', 'pub_time', 'update_time')
     search_fields = ('title',)
 
-    def save_model(self, request, obj, form, change):
-        if not obj.id:
-            obj.author = request.user.username
-        obj.save()
-
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category, CategoryAdmin)
