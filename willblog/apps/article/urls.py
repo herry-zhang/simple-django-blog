e5f0a8c782ed6dbf778cc4c6783910d6427af477
willtunner <hellohanke@outlook.com>
2017-05-10 00:02:24 +0800

    Adjust the directory structure

--- willblog/apps/article/urls.py
+++ willblog/apps/article/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url
 from willblog.apps.article import views
 
 urlpatterns = [
-    url(r'^$', home, name='index'),
+    url(r'^$', views.home, name='index'),
     url(r'(?P<pk>\d+)/$', views.detail, name='detail'),
     url(r'^feed\.xml$', LatestArticleFeed(), name='feed'),
     # url(r'edit/$', views.edit, name='edit'),
