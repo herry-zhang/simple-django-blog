663c1f9f04bb5445037c06b4127dba1d7804bbcc
willtunner <hanke.amadeus@gmail.com>
2017-06-22 16:48:52 +0800

    add gevent support

--- willblog/wsgi.py
+++ willblog/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+from gevent import monkey
 from django.core.wsgi import get_wsgi_application
+
+monkey.patch_all()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "willblog.settings.prod")
 
